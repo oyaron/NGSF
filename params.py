4a12dbd4f64d81a8851f778d3ff13cf9a6d279f6
samanthagoldwasser25 <48879389+samanthagoldwasser25@users.noreply.github.com>
2020-09-06 16:36:46 +0300

    Add files via upload

--- params.py
+++ params.py
@@ -12,30 +12,49 @@ from SF_functions import *
 
 
 
-#Saving paths
+# Choose saving paths for binned data and results 
 
-save_bin_path     = "/home/idoi/Dropbox/superfit/binned_files/"
+save_bin_path     = "/home/sam/Dropbox (Weizmann Institute)/superfit/new/something/"
 
-save_results_path = "/home/idoi/Dropbox/superfit/results/"
+save_results_path = "/home/sam/Dropbox (Weizmann Institute)/superfit/new/something/"
 
-show = False   #show plots after optimization (if False, plots will still be saved as long as)
+# Path where library folder is located (the binnings folder)
 
-# Select a range and number of steps for z and A_v
+path = "/home/sam/Dropbox (Weizmann Institute)/superfit/new/"
 
-z_num    = 11
+show = True   #show plots after optimization (if False, plots will still be saved as long as)
+
+
+#--------------------------------------------------------------------------------------------------
+
+# Select a range and number of steps for z
+
+z_start = 0  
+z_end   = 0.1
+z_num    = 21
+
+
+# Number of steps for A_v (do not change)
+
 alam_num = 21
 
-redshift      =    np.linspace(0,0.1,z_num)
-#redshift = np.array([0.065712])
+
+redshift      =    np.linspace(z_start, z_end,z_num)
 extconstant   =    np.linspace(-2,2,alam_num)
           
 
+
+# What part of the library do you want to look at?  
+
+temp_gal_tr = ['/E','/S0','/Sa','/Sb','/SB1','/SB2','/SB3','/SB4','/SB5','/SB6','/Sc']
+
+temp_sn_tr  = ['/Ia/','/Ib/','/Ic/','/II/','/Others/']
 
 
 
 # Select a wavelength range and resolution
 
-resolution = 20 #Angstrom
+resolution = 30 #Angstrom
 upper      = 10500
 lower      = 3000
 interval   = int((upper - lower)/resolution)
@@ -43,18 +62,15 @@ interval   = int((upper - lower)/resolution)
 lam        =     np.linspace(lower, upper, interval)
 
 
+# Select kind of error spectrum ('SG', 'linear' or 'included')
+
+kind = 'SG'
+
+#--------------------------------------------------------------------------------------------------
 
 
 
-
-
-#Select template library
-
-#Path where the binnings folder is, in order to pull the files from the library 
-
-path = "/home/idoi/Dropbox/superfit/"
-location_multiobj = 'multi_object/'
-
+#Template library
 
 templates_gal = glob.glob('binnings/'+ str(resolution) +'A/gal/*')
 templates_gal = [x for x in templates_gal if 'CVS' not in x and 'README' not in x]
@@ -66,22 +82,10 @@ templates_sn = [x for x in templates_sn if 'CVS' not in x and 'README' not in x]
 templates_sn = np.array(templates_sn)
 
 
-
-
-#Select which parts of the library to look at
-
-temp_gal_tr = ['/E','/S0','/Sa','/Sb','/SB1','/SB2','/SB3','/SB4','/SB5','/SB6','/Sc']
-
-temp_sn_tr  = ['/Ia/','/Ib/','/Ic/','/II/','/Others/']
-
 templates_sn_trunc = select_templates(templates_sn, temp_sn_tr)
 templates_gal_trunc = select_templates(templates_gal, temp_gal_tr)
 
 
-
-#Select type of error spectrum
-
-kind = 'SG'
 
 
 
