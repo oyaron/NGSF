e86f1db9f5df0bebabf5cf13163e0e41be5c7e67
Ido Irani <ido.irani@weizmann.ac.il>
2020-10-11 16:26:14 +0300

    changed params

--- params.py
+++ params.py
@@ -20,7 +20,6 @@ save_results_path = "something/"
 
 # Path where library folder is located (the binnings folder)
 
-<<<<<<< HEAD
 path = "/home/idoi/Dropbox/superfit/"
 
 show = True   #show plots after optimization (if False, plots will still be saved as long as)
@@ -31,18 +30,6 @@ show = True   #show plots after optimization (if False, plots will still be save
 # Select a range and number of steps for z
 
 z_start = 0  
-=======
-path  = ""
-
-show = True   #show plots after optimization (if False, plots will still be saved as long as)
-
-
-#--------------------------------------------------------------------------------------------------
-
-# Select a range and number of steps for z
-
-z_start = 0 
->>>>>>> sam-dev
 z_end   = 0.1
 z_num    = 11
 
@@ -52,13 +39,7 @@ z_num    = 11
 alam_num = 21
 
 
-<<<<<<< HEAD
 redshift      =    np.linspace(z_start, z_end,z_num)
-=======
-#redshift      =    np.linspace(z_start, z_end,z_num)
-redshift = np.array([0.108])
-
->>>>>>> sam-dev
 extconstant   =    np.linspace(-2,2,alam_num)
 #extconstant = np.array([0, 2, -2, 0])         
 
@@ -66,67 +47,14 @@ extconstant   =    np.linspace(-2,2,alam_num)
 # What part of the library do you want to look at?  
 
 temp_gal_tr = ['/E','/S0','/Sa','/Sb','/SB1','/SB2','/SB3','/SB4','/SB5','/SB6','/Sc']
-<<<<<<< HEAD
 
-temp_sn_tr  = ['/Ia/','/Ib/','/Ic/','/II/','/Others/']
-
-=======
-#temp_sn_tr = ['TDE H/2018hyz/']
-
-
-#temp_sn_tr  = ['/Ia/','/Ib/','/Ic/','/II/','/Others/']
-
-#t#emp_sn_tr = ['/TDE H/']
-
-
-#temp_sn_tr = ['/Ia 99aa-like/**/*']
-
-temp_sn_tr = ['/TDE H/',
- '/Ia-CSM-(ambigious)/',
- '/Ia-norm/',
- '/SLSN-IIn/',
- '/FBOT/',
- '/SN - Imposter/',
- '/IIb-flash/',
- '/IIn/',
- '/IIb/',
- '/Ia-rapid/',
- '/computed/',
- '/SLSN-Ib/',
- '/ILRT/',
- '/Ibn/',
- '/TDE He/',
- '/Ic-pec/',
- '/Ia 91T-like/',
- '/Ia-CSM/',
- '/SLSN-I/',
- '/Ia 99aa-like/',
- '/Ia-pec/',
- '/"super chandra"/',
- '/Ca-Ia/',
- '/SLSN-II/',
- '/Ic/',
- '/Ib/',
- '/Ia 91bg-like/',
- '/SLSN-IIb/',
- '/Ca-Ibc/',
- '/II/',
- '/Ia 02es-like/',
- '/II-flash/',
- '/Ia-02cx like/',
- '/Ic-BL/',
- '/TDE H+He/']
+temp_sn_tr = os.listdir(path + 'bank/original resolution/sne/')
  
->>>>>>> sam-dev
 
 
 # Select a wavelength range and resolution
 
-<<<<<<< HEAD
-resolution = 30 #Angstrom
-=======
 resolution = 10 #Angstrom
->>>>>>> sam-dev
 upper      = 10500
 lower      = 3000
 interval   = int((upper - lower)/resolution)
@@ -145,49 +73,27 @@ plotting = 1
 # How many top results so plot? 
 
 n = 3
-<<<<<<< HEAD
-
-#--------------------------------------------------------------------------------------------------
-
-#Template library
-
-=======
 
 #--------------------------------------------------------------------------------------------------
 
 #Template library
 #print(path + 'binnings/'+ str(resolution) +'A/gal/*')
->>>>>>> sam-dev
 templates_gal = glob.glob(path + 'binnings/'+ str(resolution) +'A/gal/*')
 templates_gal = [x for x in templates_gal if 'CVS' not in x and 'README' not in x]
 templates_gal = np.array(templates_gal)
 #print(templates_gal)
 
-<<<<<<< HEAD
-
-templates_sn = glob.glob(path + 'binnings/' + str(resolution) + 'A/sne/**/*')
-templates_sn = [x for x in templates_sn if 'CVS' not in x and 'README' not in x]
-=======
 templates_sn = glob.glob(path + 'binnings/' + str(resolution) + 'A/sne/**/**/*')
 #templates_sn = [x for x in templates_sn if 'CVS' not in x and 'README' not in x]
 templates_sn = [x for x in templates_sn if 'wiserep_spectra.csv' not in x and 'info' not in  x and 'photometry' not in x and 'photometry.pdf' not in x]
->>>>>>> sam-dev
 templates_sn = np.array(templates_sn)
 
 #print(templates_sn)
 
-<<<<<<< HEAD
-=======
-
->>>>>>> sam-dev
 templates_sn_trunc = select_templates(templates_sn, temp_sn_tr)
 templates_gal_trunc = select_templates(templates_gal, temp_gal_tr)
 
 
-<<<<<<< HEAD
-
-=======
 #print(templates_sn_trunc)
->>>>>>> sam-dev
 
 
