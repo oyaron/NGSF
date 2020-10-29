04613fbf8e3076971ce198bf57dddc0ac21cf609
Ido Irani <ido.irani@weizmann.ac.il>
2020-10-29 16:37:30 +0200

    corrected problem

--- superfit/SF_functions.py
+++ superfit/SF_functions.py
@@ -86,30 +86,6 @@ def Alam(lamin):
 
 
 
-def select_templates(DATABASE, TYPES):
-
-    
-    '''
-    
-    
-    Selects templates of a given type(s) from a template database
-   
-    Input: DATEBASE   list of templates
-           TYPES      which types should be selected
-  
-    Output: array of templates of given type(s)
-    
-    
-    '''     
-    
-    
-    database_trunc = list([])
-    
-    for type in TYPES:
-        database_trunc += list([x for x in DATABASE if type in x])
-    
-    return np.array(database_trunc)
-
 
 # ## Error choice
 
