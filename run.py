e4eaf69dbe37b7cd0f861cb930bd181356c5784f
samanthagoldwasser25 <48879389+samanthagoldwasser25@users.noreply.github.com>
2021-08-25 09:58:40 +0300

    Update run.py

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from params import *
 import numpy as np 
 import json 
 import warnings
-warnings.filterwarnings('ignore')
+#warnings.filterwarnings('ignore')
 
 with open("parameters.json", "r") as read_file:
     data = json.load(read_file)
