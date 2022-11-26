1e3fe11abb49ce6f513f0e1fe4b05d0a8ec327fb
Michael Coughlin <michael.w.coughlin@gmail.com>
2022-11-26 16:43:31 -0600

    Add auto download

--- NGSF/tests/fit.py
+++ NGSF/tests/fit.py
@@ -4,6 +4,7 @@ import os
 import pandas as pd
 import subprocess
 import sys
+import zipfile
 
 
 def test_fit():
@@ -12,6 +13,16 @@ def test_fit():
     SUPERFIT_PARAMETERS_JSON = "./parameters.json"
     SUPERFIT_DATA_PATH = "./NGSF/tests/data"
     filebase = "SN2021urb_2021-08-06_00-00-00_Keck1_LRIS_TNS"
+    NGSF_bank = "https://www.wiserep.org/sites/default/files/supyfit_bank.zip"
+    NGSF_zip = f"{SUPERFIT_PATH}/{NGSF_bank.split('/')[-1]}"
+    BANK_PATH = f"{SUPERFIT_PATH}/bank"
+
+    if not os.path.isdir(BANK_PATH):
+        curl_command = f'curl -L -H "Content-Type: application/json" -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36" -o {NGSF_zip} {NGSF_bank}'
+        os.system(curl_command)
+
+        with zipfile.ZipFile(NGSF_zip, "r") as z:
+            z.extractall(SUPERFIT_PATH)
 
     params = json.loads(open(SUPERFIT_PARAMETERS_JSON).read())
     params["object_to_fit"] = f"{SUPERFIT_DATA_PATH}/{filebase}.flm"
